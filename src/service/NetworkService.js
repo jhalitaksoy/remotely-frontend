24bfc45765c18addfd4e62a2783c66fc8956eef4
jhalitaksoy <jhalitaksoy@gmail.com>
2020-11-02 00:47:02 +0300

    Changed server ip to https

--- src/service/NetworkService.js
+++ src/service/NetworkService.js
@@ -5,7 +5,8 @@ const { default: Axios } = require("axios");
 export function serverUrl() {
     //return 'http://192.168.43.2:80' 
     //return 'http://127.0.0.1:8080'
-    return 'http://34.107.125.219'
+    //return 'http://34.107.125.219'
+    return 'https://34.107.125.219'
 }
 
 function headers() {
